b74f6cda512de0973dca1aed8ac334c5e064f6ac
Pavel Yudin <pasha.judin@gmail.com>
2021-02-17 19:46:24 +0200

    removed api files

--- pages/api/addPost.tsx
+++
@@ -1,6 +0,0 @@
-const { writeFile } = require("fs").promises;
-
-export default (req, res) => {
-  writeFile(`${__dirname}/test.json`, "New post", { encoding: "utf8" });
-  res.status(200).json({ name: "New post" });
-};
